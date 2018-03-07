ffb560ddcbe1f5e3b0c0e908fa8d994b93dc0294
kuari <kuari@justmylife.cc>
2018-03-07 11:46:05 +0800

    python3_cgi

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 * i3: config i3status.conf
 * xinitrc
 * music.py
+* python3_cgi
---
+++ python3_cgi.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from http.server import CGIHTTPRequestHandler, test
+    
+test(CGIHTTPRequestHandler)
